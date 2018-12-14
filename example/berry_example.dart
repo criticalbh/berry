194d3ceeac4e3811687236e064cf1f977dbf90a7
Admir Sabanovic <admir.sabanovic@moduscreate.com>
2018-12-14 13:33:54 +0100

    initialization

---
+++ example/berry_example.dart
@@ -0,0 +1,6 @@
+import 'package:berry/berry.dart';
+
+main() {
+  var awesome = new Awesome();
+  print('awesome: ${awesome.isAwesome}');
+}
---
+++ lib/berry.dart
@@ -0,0 +1,8 @@
+/// Support for doing something awesome.
+///
+/// More dartdocs go here.
+library berry;
+
+export 'src/berry_base.dart';
+
+// TODO: Export any libraries intended for clients of this package.
